6da1bc18958990cfbaf29aec929ac7b2341640f4
Ngomso Gaya <125261321+NgomsoGaya@users.noreply.github.com>
2025-01-04 20:47:32 +0200

    offers component

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from '../Components/Hero/Hero.jsx';
-import Popular  from '../Components/Popular/Popular.jsx'; // Ensure the component starts with an uppercase letter
+import Popular  from '../Components/Popular/Popular.jsx';
+import { Offers } from '../Components/Offers/Offers.jsx';
 
 export const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
     </div>
   );
 };
