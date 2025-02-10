54aee7b96d77f4bf37d0d30f0419a59ac7d14b30
Ngomso Gaya <125261321+NgomsoGaya@users.noreply.github.com>
2025-02-10 09:48:13 +0200

    modified timing for the hero and sitemap

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from '../Assets/hello.png'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/banner2.jpg'
-import plcholderhero from '../Assets/heroplcholder.jpg'
+import React, { useEffect } from 'react';
+import './Hero.css';
+import hand_icon from '../Assets/hello.png';
+import arrow_icon from '../Assets/arrow.png';
+import hero_image from '../Assets/banner2.jpg';
 
 export const Hero = () => {
+  
+  useEffect(() => {
+    const img = document.querySelector('.hero-img');
+    if (img) {
+      img.onload = () => {
+        console.log('Hero image loaded:', img.src);
+      };
+    }
+  }, []);
+
   return (
     <div className='hero'>
-        <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
-            <div>
-                <div className="hero-hand-icon">
-                    <p>new</p>
-                    <img src={hand_icon} alt="" />
-                </div>
-                <p>collections</p>
-                <p>for everyone</p>
-            </div>
-            <div className="hero-latest-btn">
-                <div>Latest Collection</div>
-                <img src={arrow_icon} alt="" />
-            </div>
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>collections</p>
+          <p>for everyone</p>
         </div>
-        <div className="hero-right">
-        <img className='hero-img' src={hero_image} alt="" />
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
         </div>
-        </div>
-  )
-}
+      </div>
+      <div className="hero-right">
+        <img className='hero-img' src={hero_image} alt="Hero Banner" />
+      </div>
+    </div>
+  );
+};
