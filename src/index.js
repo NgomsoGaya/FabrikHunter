fffede4a4d0c2a595178b4e3cb79bcd2571261a9
Ngomso Gaya <125261321+NgomsoGaya@users.noreply.github.com>
2025-05-09 13:07:30 +0200

    added braze

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
+import * as braze from '@braze/web-sdk';
+
+braze.initialize("9bd6172a-c3f7-43db-a903-6d27d739aa66", {
+  baseUrl: "sdk.fra-01.braze.eu",
+  enableLogging: true,
+  enableAutomaticPushRegistration: true,
+  enableHtmlInAppMessages: true,
+  enableAutomaticSessionTracking: true,
+  sessionTimeout: 30,
+  enableGeofencing: true,
+  geofencingEnabled: true,
+  geofencingRegionMonitoringEnabled: true,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
