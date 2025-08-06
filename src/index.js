e50f7e215777cbdb2c34bf69ed2c532d588b5049
Ngomso Gaya <125261321+NgomsoGaya@users.noreply.github.com>
2025-08-06 15:14:32 +0200

    removed braze sdk

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
 import * as braze from '@braze/web-sdk';
 
-braze.initialize("9bd6172a-c3f7-43db-a903-6d27d739aa66", {
-  baseUrl: "sdk.fra-01.braze.eu",
-  enableLogging: true,
-  enableAutomaticPushRegistration: true,
-  enableHtmlInAppMessages: true,
-  enableAutomaticSessionTracking: true,
-  sessionTimeout: 30,
-  enableGeofencing: true,
-  geofencingEnabled: true,
-  geofencingRegionMonitoringEnabled: true,
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
